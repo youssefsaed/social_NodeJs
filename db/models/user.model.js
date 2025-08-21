2e364d5acb81e3b0c3baf67cf50f1fe1a69f1077
youssef <youssef@gmail.com>
2025-08-21 19:15:50 +0300

    upload

--- db/models/user.model.js
+++ db/models/user.model.js
@@ -7,24 +7,27 @@ const userSchema = new mongoose.Schema({
     Username: String,
     Email: String,
     password: String,
-    profilPicture:{
-        type:String,
-        default:''
+    profilPicture: {
+        type: String,
+        default: ''
     },
-    profilPictureCover:{
-        type:String,
-        default:''
+    profilPictureCover: {
+        type: String,
+        default: ''
     },
     confirmed: {
         type: Boolean,
         default: false
     },
-    isLoggedIn:Boolean,
-    otpCode:String
+    isLoggedIn: Boolean,
+    otpCode: String
 }, { timestamps: true })
 
-userSchema.pre('save',function(){
-    this.password=bcrypt.hashSync(this.password,+process.env.SALT_ROUNDS)
+userSchema.pre('save', function () {
+    this.password = bcrypt.hashSync(this.password, +process.env.SALT_ROUNDS)
+})
+userSchema.post('init', (doc) => {
+    doc.profilPicture ='https://socialnodejs-production.up.railway.app/social/uploads/'+ doc.profilPicture
 })
 
 export const userModel = mongoose.model('user', userSchema)
