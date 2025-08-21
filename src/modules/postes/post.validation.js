1e64a824ffae48129baf8b7355fd6e245859f214
youssef <youssef@gmail.com>
2025-08-21 19:17:00 +0300

    upload

--- src/modules/postes/post.validation.js
+++ src/modules/postes/post.validation.js
@@ -1,34 +1,33 @@
 import Joi from "joi";
 /////////////////////////////////////validation add post
-export const addPostSchema={
-    body:Joi.object().required().keys({
-        caption:Joi.string().required()
-    })
-}
+export const addPostSchema = Joi.object().required().keys({
+    caption: Joi.string().required()
+})
+
 /////////////////////////////////////validation update post status
-export const updateStatusSchema={
-    query:Joi.object().required().keys({
-        id:Joi.string().hex().length(24).required()
+export const updateStatusSchema = {
+    query: Joi.object().required().keys({
+        id: Joi.string().hex().length(24).required()
     })
 }
 /////////////////////////////////////validation update post 
-export const updatePostSchema={
-    body:Joi.object().keys({
-        caption:Joi.string()
+export const updatePostSchema = {
+    body: Joi.object().keys({
+        caption: Joi.string()
     }),
-    query:Joi.object().required().keys({
-        id:Joi.string().hex().length(24).required()
+    query: Joi.object().required().keys({
+        id: Joi.string().hex().length(24).required()
     })
 }
 ////////////////////////////////////////validation delete post
-export const deletePostSchema={
-    query:Joi.object().required().keys({
-        id:Joi.string().hex().length(24).required()
+export const deletePostSchema = {
+    query: Joi.object().required().keys({
+        id: Joi.string().hex().length(24).required()
     })
 }
 ////////////////////////////////////////validation like and unlike
-export const likeAunlikeSchema={
-    query:Joi.object().required().keys({
-        id:Joi.string().hex().length(24).required()
+export const likeAunlikeSchema = {
+    query: Joi.object().required().keys({
+        id: Joi.string().hex().length(24).required()
     })
 }
